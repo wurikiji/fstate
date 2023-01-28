9561168bb948914be775a89ec049f729cf052031
wurikiji <wurikiji@gmail.com>
2023-01-28 17:55:32 +0900

    init: initialize repo

---
+++ packages/flutter_fstate/lib/flutter_fstate.dart
@@ -0,0 +1,7 @@
+library flutter_fstate;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ packages/fstate/lib/fstate.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library fstate;
+
+export 'src/fstate_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
