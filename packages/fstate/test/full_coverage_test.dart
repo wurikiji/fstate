f4221a2b4150ea660bdbfb8ab0897e551ed4d9fd
wurikiji <wurikiji@gmail.com>
2023-03-02 14:14:30 +0900

    chore: remove full coverage file

--- packages/fstate/test/full_coverage_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-// ignore_for_file: unused_import
-import 'package:fstate/fstate.dart';
-import 'package:fstate/src/foundation/annotations.dart';
-import 'package:fstate/src/foundation/container.dart';
-import 'package:fstate/src/widgets/scoped_fstate_container.dart';
-
-void main() { test("", () => null); }
