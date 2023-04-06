50959bb0307f7f750c14ab2ad9c071a6f74baf21
wurikiji <wurikiji@gmail.com>
2023-04-07 01:14:05 +0900

    chore: remove unused file

--- packages/fstate_generator/lib/src/type_checkers/key.dart
+++
@@ -1,9 +0,0 @@
-import 'package:source_gen/source_gen.dart';
-
-TypeChecker fstateKeyChecker = TypeChecker.fromUrl(
-  'package:fstate/src/foundation/key.dart#FstateKey',
-);
-
-TypeChecker fstateKeyFamilyAnnotationChecker = TypeChecker.fromUrl(
-  'package:fstate/src/foundation/key.dart#FstateKeyFamily',
-);
